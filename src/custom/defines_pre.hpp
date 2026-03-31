2796e767ccef677390ee40a89ce15c747e2d5b44
Demonbytes-lab <galeph88@gmail.com>
2026-03-31 08:32:38 +0200

    adjusting packetver

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -9,6 +9,9 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 
+// NoxaraRO: WARP0716 client (Ragexe 2025-07-16)
+#define PACKETVER 20250716
+
 
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
